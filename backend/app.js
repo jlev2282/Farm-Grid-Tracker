691c6d41167cfd769525a2fec309c4e71597089a
John Leverett <jlev2282@gmail.com>
2024-06-23 20:51:58 -0400

    renamed server to app

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'frontend')));
@@ -22,3 +24,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
